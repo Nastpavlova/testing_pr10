f06f97b774a83650d423f1200a03e7dac6828a67
Nastpavlova <nastpavlovax@mail.ru>
2025-08-28 12:52:07 +0300

    SideBar can open. Initial State of ArrowButton

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,31 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState } from 'react'
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpenBar, setIsOpenBar] = useState(false);
+
+	const toggleOpenBar = () => setIsOpenBar(prev => !prev);
+
+	const moveSideBar = () => {
+		// lдобавить aside .container_open
+	}
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
+			<ArrowButton isOpen={isOpenBar} onClick={toggleOpenBar} />
+			{isOpenBar && (
+				<aside className={`${styles.container} ${isOpenBar ? styles.container_open : ''} `}>
+					<form className={styles.form}>
+						<div className={styles.bottomContainer}>
+							<Button title='Сбросить' htmlType='reset' type='clear' />
+							<Button title='Применить' htmlType='submit' type='apply' />
+						</div>
+					</form>
+				</aside>
+			)}
 		</>
 	);
 };
