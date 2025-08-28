34d0a0a17c94525dbb1e8f16bfd0c8ca8f5f76c8
Nastpavlova <nastpavlovax@mail.ru>
2025-08-28 13:00:53 +0300

    edit comment

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm /> /** Форма редактирования свойств **/
+			<ArticleParamsForm /> {/* Форма редактирования свойств */}
 			<Article />
 		</main>
 	);
